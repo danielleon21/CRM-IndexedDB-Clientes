f4942967028ac5ad541af03e5e5d85b19711a28c
Daniel León <matusleonc@outlook.com>
2022-09-21 17:00:37 -0500

    added functionality to add clients to DB

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -40,6 +40,39 @@
             imprimirAlerta('Todos los campos son obligatorios', 'error')
             return
         }
+
+        // Crear un objeto con la informacion 
+
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa,
+            id: Date.now()
+        }
+
+        crearNuevoCliente(cliente)
+
+    }
+
+    function crearNuevoCliente(cliente) {
+        const transaction = DB.transaction(['crm'], 'readwrite')
+
+        const objectStore = transaction.objectStore('crm')
+
+        objectStore.add(cliente)
+
+        transaction.onerror = function () {
+            imprimirAlerta('Hubo un error', 'error')
+        }
+
+        transaction.oncomplete = function () {
+            imprimirAlerta('El Cliente se agrego correctamente')
+        }
+
+        setTimeout(() => {
+            window.location.href = "index.html"
+        }, 3000);
     }
 
     function imprimirAlerta(mensaje, tipo) {
@@ -65,8 +98,6 @@
                 divAlerta.remove()
             }, 3000);
         }
-
-
     }
 
 })()
